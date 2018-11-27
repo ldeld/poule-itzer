8293ba43b35c3d204588fa2808d4bf8189a8fc04
Lorenzo <lorenzo@tilkee.com>
2018-11-27 23:07:02 +0100

    rails webpack:install

--- app/controllers/cocorocos_controller.rb
+++ app/controllers/cocorocos_controller.rb
@@ -1,8 +1,14 @@
 class CocorocosController < ApplicationController
   def new
+    @cocoroco = Cocoroco.new
   end
 
   def create
+    if @cocoroco.save
+      redirect_to new_cocoroco_page, notice: 'Succesfully created'
+    else
+      render :new
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'cocorocos/new'
-
-  get 'cocorocos/create'
-
-  get 'cocorocos/show'
+  resources :cocorocos, only: %i[new index create show]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
